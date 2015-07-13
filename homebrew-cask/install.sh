53e0190f08f9081be6d6c7255531a5f660991228
Derek Keith <derek@mojotech.com>
2015-07-13 10:13:24 -0600

    Update

--- homebrew-cask/install.sh
+++ homebrew-cask/install.sh
@@ -5,7 +5,7 @@
 # This install homebrew casks that I prefer
 
 # Install homebrew casks
-brew cask install 1password adobe-creative-cloud alfred appcleaner bartender boom caffeine contexts divvy dropbox\
+brew cask install 1password adobe-creative-cloud alfred appcleaner bartender boom caffeine cheatsheet contexts divvy dropbox\
   firefox flux gfxcardstatus goofy google-chrome hammerspoon istat-menus iterm2\
   karabiner macdown mailbox paw postico seil slack spotify sublime-text the-unarchiver\
   totalfinder totalspaces transmission transmit virtualbox vlc xquartz
